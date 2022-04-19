579cc4cb12baa2478b4685fbce023c8319ccad76
karen banci <karenbanci1@gmail.com>
2022-04-18 17:18:46 -0700

    init commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import './App.css';
+
+function App() {
+  render();{
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        {/* <!-- Add a SearchBar component --> */}
+        <div className="App-playlist">
+          {/* <!-- Add a SearchResults component --> */}
+          {/* <!-- Add a Playlist component --> */}
+        </div>
+      </div>
+    </div>
+  }
+}
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './Components/App/App.js';
+import './Components/App/App.css';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
